d71f679401643de5af060d91c3c1bfbc6f14e49e
Sparsh Chandra <sparshchandra@sparshs-air.myfiosgateway.com>
2023-09-11 19:14:32 -0400

    minor fix

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,13 +1,9 @@
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import { useRef } from 'react'
 import LogoS from '../../../assets/images/sparsh.png'
 import './index.scss'
 
 const Logo = () => {
   const bgRef = useRef()
-  const outlineLogoRef = useRef()
-  const solidLogoRef = useRef()
 
   return (
     <div className="logo-container" ref={bgRef}>
